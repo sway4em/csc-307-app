c15943ce2a2a7c049e5dbba1f15b05cacdf30055
Swayam Chidrawar <schidraw@calpoly.edu>
2025-01-12 21:06:26 -0800

    completed part 6 of IE1

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -4,6 +4,7 @@ function TableHeader() {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Remove</th>
         </tr>
       </thead>
     );
@@ -12,10 +13,15 @@ function TableHeader() {
   function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
       return (
-        <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
-        </tr>
+<tr key={index}>
+  <td>{row.name}</td>
+  <td>{row.job}</td>
+  <td>
+    <button onClick={() => props.removeCharacter(index)}>
+      Delete
+    </button>
+  </td>
+</tr>
       );
      }
     );
@@ -30,7 +36,10 @@ function Table(props) {
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={props.characterData} />
+        <TableBody
+        characterData={props.characterData}
+        removeCharacter={props.removeCharacter}
+      />
       </table>
     );
 }
