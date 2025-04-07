45714bff3151855eff75a0e4f79dc4f1d454730e
Swayam Chidrawar <schidraw@calpoly.edu>
2025-04-06 22:52:41 -0700

    IE1 part 4

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -11,36 +11,30 @@ function TableHeader() {
     );
   }
   
-function TableBody() {
+  function TableBody(props) {
+    const rows = props.characterData.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.name}</td>
+          <td>{row.job}</td>
+        </tr>
+      );
+     }
+    );
     return (
         <tbody>
-        <tr>
-            <td>Charlie</td>
-            <td>Janitor</td>
-        </tr>
-        <tr>
-            <td>Mac</td>
-            <td>Bouncer</td>
-        </tr>
-        <tr>
-            <td>Dee</td>
-            <td>Aspiring actress</td>
-        </tr>
-        <tr>
-            <td>Dennis</td>
-            <td>Bartender</td>
-        </tr>
-        </tbody>
-    );
-}
-
-function Table() {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody />
-      </table>
-    );
-}
+          {rows}
+         </tbody>
+     );
+  }
+  
+  function Table(props) {
+      return (
+        <table>
+          <TableHeader />
+          <TableBody characterData={props.characterData} />
+        </table>
+      );
+  }
 
 export default Table;
