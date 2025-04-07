7b5612bf914a1fec80dc6cb212b92f736686c8fc
Swayam Chidrawar <schidraw@calpoly.edu>
2025-04-06 22:44:02 -0700

    IE1 part 1

---
+++ packages/react-frontend/src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import "./main.css";
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+}
+
+// Create the container
+const container = document.getElementById("root");
+
+// Create a root
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the Root
+root.render(<MyApp />);
