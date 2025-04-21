9328cb6bfa72636450e130085366ca4a25991ab9
Swayam Chidrawar <schidraw@calpoly.edu>
2025-04-20 23:12:19 -0700

    IE3 part 4.1.2

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -21,20 +21,25 @@ function MyApp() {
   }
 
   function postUser(person) {
-    const promise = fetch("Http://localhost:8000/users", {
+    const promise = fetch("http://localhost:8000/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(person),
     });
-
     return promise;
   }
 
   function updateList(person) {
     postUser(person)
-      .then(() => setCharacters([...characters, person]))
+      .then((response) => {
+        if (response.status === 201) {
+          setCharacters([...characters, person]);
+        } else {
+          console.log("User creation failed: Status code is not 201");
+        }
+      })
       .catch((error) => {
         console.log(error);
       });
