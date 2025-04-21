776212642834748937e66d3e3406d101a52a9c65
Swayam Chidrawar <schidraw@calpoly.edu>
2025-04-20 23:10:38 -0700

    IE3 part 4.1

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -96,7 +96,7 @@ const addUser = (user) => {
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
-  res.send();
+  res.status(201).send(userToAdd);
 });
 
 const deleteUser = (id) => {
